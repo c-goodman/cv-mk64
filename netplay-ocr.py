4210a04af20b2761c68bf8d80e3ca53c17e38814
Cooper Goodman <c-goodman@tamu.edu>
2021-09-11 16:11:54 -0500

    Wrangles data from easyocr into clean Pandas DataFrame

--- netplay-ocr.py
+++ netplay-ocr.py
@@ -1,9 +1,11 @@
+from logging import StringTemplateStyle
 import cv2 as cv
 import numpy as np
 import os
 from windowcapture import WindowCapture
 import win32gui, win32ui, win32con
 import easyocr
+import pandas as pd
 
 
 # # Change the working directory to the folder this script is in.
@@ -13,7 +15,34 @@ import easyocr
 reader = easyocr.Reader(lang_list=['en'])
 wincap = WindowCapture("Dolphin NetPlay")
 screenshot = wincap.get_screenshot()
-crop_img = screenshot[44:497, 455:739]
-simple_output = reader.readtext(crop_img, detail=0)
+
+# Crop position if hosting
+crop_img = screenshot[88:497, 455:739]
+# Crop position if not hosting
+# crop_img = screenshot[44:497, 455:739]
+simple_output = reader.readtext(crop_img, detail=0, mag_ratio=2)
         
-print(simple_output)
+
+# simple_output_testing = ['cooper[1]', '5,0 Win | 1---', 'Oms', 'connor[2]', '5,0 Win |-2-----', '8ms',
+#  'cole[3]', '5.0 Win | -3---', '19ms', 'Ping:', 'Ping:', 'Ping:']
+
+# Remove items from list that end in 'ms'
+no_endswith_ms = [s for s in simple_output if not s.endswith('ms')]
+# Remove items from list that end in 'ing:'
+no_endswith_ing = [s for s in no_endswith_ms if not s.endswith('ing:')]
+
+# Divide into two lists based on .startswith method
+ports = [s for s in no_endswith_ing if s.startswith('5')]
+players = [s for s in no_endswith_ing if not s.startswith('5')]
+
+# Create dictionary of new lists
+diction = {'Players':players, 'Ports':ports}
+
+# Put dictionary in a Pandas DataFrame
+df = pd.DataFrame(diction)
+
+# Use .strip method to remove clutter
+df['Players'] = df['Players'].str.strip('[]12345678')
+df['Ports'] = df['Ports'].str.strip('5,.0 |-Win')
+
+print(df)
